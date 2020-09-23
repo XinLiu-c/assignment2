f8d2db7e91f6d5c1c20ba607afc52e781a638641
xinliu-c <2323953781@qq.com>
2020-09-23 19:53:17 +0800

    mark weather it is visited or not

--- place.py
+++ place.py
@@ -1,6 +1,6 @@
 """..."""
-
-
+from operator import itemgetter
+import csv
 # Create your Place class in this file
 
 
@@ -13,8 +13,30 @@ class Place:
         self.visited_status=visited_status
 
     def __str__(self):
-        return "{} ({}) with priority {} visited status: {}".format(self.name, self.country, self.priority,
-                                                                    self.visited_status)
+        return "{} ({}) with priority {} visited status: {}".format(self.name, self.country, self.priority,self.visited_status)
+
+    def mark_places(self):
+        list_data = []  # empty list
+        filename = "places.csv"
+        input_file = open(filename, 'r')
+        for line in input_file:
+            line = line.strip()  # Remove the \n
+            parts = line.split(',')  # Separate the data into its parts
+            list_data.append(parts)
+        input_file.close()
+        list_data.sort(key=itemgetter(3))  # sort data according to "n" and "v"
+        list_data.sort(key=itemgetter(2), reverse=True)  # sort data according to the priority number
+        count = 1
+        for list in list_data:
+            if list[3] == "n":
+                print("*{}. {:9} in {:12} priority {:2}".format(count, list[0], list[1], list[2]))
+                count += 1
+            else:
+                print(" {}. {:9} in {:12} priority {:2}".format(count, list[0], list[1], list[2]))
+                count += 1
+        return list_data
+
+
 
 
 
