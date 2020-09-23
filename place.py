a304e2be307aaa6f9984c417f6627f245306d95e
xinliu-c <2323953781@qq.com>
2020-09-23 17:20:28 +0800

    __int__

--- place.py
+++ place.py
@@ -6,4 +6,11 @@
 
 class Place:
     """..."""
-    pass
+    def __int__(self,name="",country="",priority=0,visited_status=""):
+        self.name=name
+        self.country=country
+        self.priority=priority
+        self.visited_status=visited_status
+
+
+
