693894cdf4eee3469fc4c350218b3454d675dc44
xinliu-c <2323953781@qq.com>
2020-09-26 23:14:36 +0800

    Test sorting - name

--- test_placecollection.py
+++ test_placecollection.py
@@ -6,32 +6,28 @@ from place import Place
 def run_tests():
     """Test PlaceCollection class."""
 
-    # Test empty PlaceCollection (defaults)
+    """Test empty PlaceCollection (defaults)"""
     print("Test empty PlaceCollection:")
     place_collection = PlaceCollection()
     print(place_collection)
-    assert not place_collection.places  # an empty list is considered False
+    assert not place_collection.list_data  # an empty list is considered False
 
-    # Test loading places
+    """Test loading places"""
     print("Test loading places:")
-    place_collection.load_places('places.csv')
+    place_collection.read_places('places.csv')
     print(place_collection)
-    assert place_collection.places  # assuming CSV file is non-empty, non-empty list is considered True
+    assert place_collection.list_data  # assuming CSV file is non-empty, non-empty list is considered True
 
-    # Test adding a new Place with values
+    """Test adding a new Place with values"""
     print("Test adding new place:")
     place_collection.add_place(Place("Smithfield", "Australia", 5, False))
     print(place_collection)
 
-    # Test sorting places
-    print("Test sorting - priority:")
-    place_collection.sort("priority")
+    """Test sorting - name:"""
+    print("Test sorting - name:")
+    place_collection.s_places("name")
     print(place_collection)
-    # TODO: Add more sorting tests
 
-    # TODO: Test saving places (check CSV file manually to see results)
-
-    # TODO: Add more tests, as appropriate, for each method
 
 
 run_tests()
