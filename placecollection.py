a9548ddc5fa5a4007c6063b4580f0138aed1cfdd
xinliu-c <2323953781@qq.com>
2020-09-26 23:10:20 +0800

    str

--- placecollection.py
+++ placecollection.py
@@ -10,6 +10,16 @@ class PlaceCollection:
     def __init__(self):
         self.list_data = []
 
+    def __str__(self):
+        after_place = ''
+        for prev_place in self.list_data:
+            for i in prev_place[:-1]:
+                after_place += str(i) + ','
+            after_place = after_place + '\n'
+
+        return after_place
+
+
 
 
 
