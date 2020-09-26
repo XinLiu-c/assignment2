10ab94ed9a093e1db435d4b0c0487495a618f23b
xinliu-c <2323953781@qq.com>
2020-09-26 23:21:12 +0800

    init

--- main.py
+++ main.py
@@ -7,11 +7,23 @@ GitHub URL:
 # Create your main program in this file, using the TravelTrackerApp class
 
 from kivy.app import App
+from placecollection import PlaceCollection
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.properties import ListProperty
+from kivy.properties import StringProperty
 
 
+states={"Name":0,"Country":1,"Priority":2,"Visited":3}
 class TravelTrackerApp(App):
-    """..."""
-    pass
+    states=ListProperty()
+    current_state=StringProperty()
+
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.place_collection = PlaceCollection()
+        self.place_collection.read_places('places.csv')
+
 
 
 if __name__ == '__main__':
