d9ff311da35e4aadcd994960bfa424f71574bd21
xinliu-c <2323953781@qq.com>
2020-09-26 23:22:19 +0800

    add places

--- main.py
+++ main.py
@@ -25,9 +25,26 @@ class TravelTrackerApp(App):
         self.place_collection.read_places('places.csv')
 
     def build(self):
+        """build app"""
         self.title="Travel Tracker"
         self.root=Builder.load_file("app.kv")
         return self.root
+
+    def add_place(self):
+        """add places"""
+        add_name=self.root.ids.add_name.text
+        add_country=self.root.ids.add_country.text
+        add_priority=self.root.ids.add_priority.text
+
+        if add_name.strip()== "" or add_country.strip()=="" or add_priority=="":
+            end="Cannot be blank!"
+        else:
+            if int(add_priority)<=0:
+                end="priority must greater than 0 !"
+            else:
+                end="{} ({}) with priority {}.".format(add_name,add_country,add_priority)
+                self.place_collection.list_data.append([add_name,add_country,add_priority],"n")
+        self.root.ids.end.text = end
 
 
 
