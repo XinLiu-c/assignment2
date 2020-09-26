b239028bb6c5934043adae98ffdd3e564b1077d3
xinliu-c <2323953781@qq.com>
2020-09-26 23:10:58 +0800

    Return a list of unvisited and visited places

--- placecollection.py
+++ placecollection.py
@@ -20,7 +20,19 @@ class PlaceCollection:
         return after_place
 
 
+    def read_places(self, filename):
+        """Return a list of unvisited and visited places"""
+        """Read file, and add data"""
+        try:
+            with open(filename, 'r') as in_file:
+                places_listed = in_file.readlines()
+                for prev_place in places_listed:
+                    prev_place=prev_place.strip().split(',')
+                    self.list_data.append(prev_place)
+                for i in self.list_data:
+                    i[2] = int(i[2])
+        except IOError:
+            print('File is not be found.')
 
-
-
+        return self.list_data
 
