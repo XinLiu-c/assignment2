77010bf438986f0f1d7c60078b379662d3fc6975
xinliu-c <2323953781@qq.com>
2020-09-26 23:16:57 +0800

    display menu

--- a1_classes.py
+++ a1_classes.py
@@ -2,4 +2,12 @@
 # Copy your first assignment to this file, then update it to use Place class
 # Optionally, you may also use PlaceCollection class
 
+
 from place import Place
+from placecollection import PlaceCollection
+
+def get_menu():
+    """display menu"""
+    choice = input("Menu\nL - List places\nA - Add new place\nM - Mark a place as visited\nQ - Quit\nYour choice:").upper()
+    return choice
+
